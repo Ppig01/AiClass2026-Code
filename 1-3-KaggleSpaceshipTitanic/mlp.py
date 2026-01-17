16b22528b4181e61c4401baae7a225ae602d319a
zixiao_bios <zixiao_bios@outlook.com>
2026-01-17 17:05:52 +0800

    1-3

---
+++ 1-3-KaggleSpaceshipTitanic/mlp.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# 定义一个简单的多层感知机模型
+class SimpleMLP(nn.Module):
+    def __init__(self, input_dim: int, hidden_num: int, hidden_dim: int, output_dim: int):
+        pass
